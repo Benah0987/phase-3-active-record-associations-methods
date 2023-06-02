8dafed095570c5f0cf627e662a91d38ce7ec7aed
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-02 23:08:50 +0300

    using find_or_create_by

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,21 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    # using the count method to get no. of songs
+    self.song.count
+
   end
 
   def artist_count
+    # use the count method
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
+    # pluck retrieves only the specified attribute from databasee
+    # it pluks names
     # return an array of strings containing every musician's name
+    self.artists.pluck(:name)
   end
 end
